56b900f579284fe12455815a643623cb0759b20c
schochastics <david@schochastics.net>
2024-02-21 19:01:05 +0100

    some functions (not working)

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,10 +1,31 @@
 use extendr_api::prelude::*;
+use phonenumber::{Mode, PhoneNumber};
 
-/// Return string `"Hello world!"` to R.
+/// Parse phone number
+// fn parse_phone(input: &str) -> PhoneNumber {
+// }
+
 /// @export
 #[extendr]
-fn hello_world() -> &'static str {
-    "Hello world!"
+fn is_valid(input: &str) -> bool {
+    let result = phonenumber::parse(None, input);
+    match result {
+        Ok(value) => phonenumber::is_valid(&value),
+        Err(e) => false,
+    }
+}
+
+/// @export
+#[extendr]
+fn extract_phone(input: &str) {
+    let result = phonenumber::parse::extract(input);
+    match result {
+        Ok((remainder, parsed)) => {
+            println!("Parsed output: {}", parsed); // Access the parsed output
+            println!("Remaining input: {}", remainder); // Access the remaining input
+        }
+        Err(error) => println!("Error encountered: {:?}", error),
+    }
 }
 
 // Macro to generate exports.
@@ -12,5 +33,6 @@ fn hello_world() -> &'static str {
 // See corresponding C code in `entrypoint.c`.
 extendr_module! {
     mod dialrs;
-    fn hello_world;
+    fn is_valid;
+    fn extract_phone;
 }
