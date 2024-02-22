4ea5311060f1cdd2833c942d24abb2542451c8ed
schochastics <david@schochastics.net>
2024-02-22 20:41:53 +0100

    added fake data

---
+++ R/data.R
@@ -0,0 +1,2 @@
+#' A set of fake phone numbers for testing
+"fake_data"
---
+++ data-raw/dataset.R
@@ -0,0 +1,53 @@
+fake_phone <- c(
+    "+1-800-123-4567", # US toll-free
+    "+44 20 7946 0958", # UK, London
+    "+49-89-636-48018", # Germany, Munich
+    "+81-3-3477-0111", # Japan, Tokyo
+    "+33 1 42 68 53 00", # France, Paris
+    "+61-2-9876-5432", # Australia, Sydney
+    "+86 10 6552 9988", # China, Beijing
+    "+91-22-2778-4040", # India, Mumbai
+    "+7 495 697-0349", # Russia, Moscow
+    "+55 21 2505-6700", # Brazil, Rio de Janeiro
+    "+27 21 401 9111", # South Africa, Cape Town
+    "+34 91 398 43 00", # Spain, Madrid
+    "+39 06 6988 4466", # Italy, Rome
+    "+46 8 5792 9100", # Sweden, Stockholm
+    "+48 22 594 19 99", # Poland, Warsaw
+    "+1-246-436-9000", # Barbados
+    "+20 2 3377 3222", # Egypt, Cairo
+    "+51 1 203-5000", # Peru, Lima
+    "+64 9-306 3000", # New Zealand, Auckland
+    "+90 212 444 0 145", # Turkey, Istanbul
+    "+358 9 310 1691", # Finland, Helsinki
+    "+82 2 2001-0114", # South Korea, Seoul
+    "+62 21 5085 1975", # Indonesia, Jakarta
+    "+974 4499 7499", # Qatar, Doha
+    "+966 11 488 3800", # Saudi Arabia, Riyadh
+    "+31 20 794 0800", # Netherlands, Amsterdam
+    "+41 44 212 44 88", # Switzerland, Zurich
+    "+54 11 4319 1111", # Argentina, Buenos Aires
+    "+603-2176 8555", # Malaysia, Kuala Lumpur
+    "+52 55 5227 4600", # Mexico, Mexico City
+    "+973 1723 8000", # Bahrain, Manama
+    "+375 17 203-34-45", # Belarus, Minsk
+    "+47 22 44 85 50", # Norway, Oslo
+    "+43 1 50110-0", # Austria, Vienna
+    "+359 2 981 981", # Bulgaria, Sofia
+    "+385 1 4563 111", # Croatia, Zagreb
+    "+357 22 867000", # Cyprus, Nicosia
+    "+420 224 491 111", # Czech Republic, Prague
+    "+372 612 3456", # Estonia, Tallinn
+    "+30 21 0330 1000", # Greece, Athens
+    "+36 1 3 111 111", # Hungary, Budapest
+    "+354 545 1100", # Iceland, Reykjavik
+    "+353 1 222 2222", # Ireland, Dublin
+    "+371 6709 7000", # Latvia, Riga
+    "+370 5 266 6666", # Lithuania, Vilnius
+    "+352 22 999 999", # Luxembourg
+    "+356 2124 1234", # Malta, Valletta
+    "+377 98 98 88 88", # Monaco
+    "+64 4-472 2249", # New Zealand, Wellington
+    "+507 507-7200" # Panama, Panama City
+)
+usethis::use_data(fake_phone, overwrite = TRUE)
